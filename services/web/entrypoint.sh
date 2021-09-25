f403e1e689c1bc96962d67792c3e618071d428c5
Michael Datz <mkdatz@gmail.com>
2021-09-24 21:12:43 -0400

    Update entrypoint.sh

--- services/web/entrypoint.sh
+++ services/web/entrypoint.sh
@@ -2,15 +2,12 @@
 
 if [ "$DATABASE" = "postgres" ]
 then
-    echo "Waiting for postgres..."
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
     done
 
-    echo "PostgreSQL started"
+    echo "Postgresql connected"
 fi
-
 python manage.py create_db
-
 exec "$@"
