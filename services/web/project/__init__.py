6b54d103c46689317ccfd704e8c2b567a75aee7f
Michael Datz <mkdatz@gmail.com>
2021-09-24 21:17:21 -0400

    Update __init__.py

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,5 +1,5 @@
 import os
-
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from flask import (
     Flask,
@@ -9,27 +9,23 @@ from flask import (
     redirect,
     url_for
 )
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
 db = SQLAlchemy(app)
 
-
 class User(db.Model):
     __tablename__ = "users"
-
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True, nullable=False)
     active = db.Column(db.Boolean(), default=True, nullable=False)
-
     def __init__(self, email):
         self.email = email
 
 
 @app.route("/")
 def hello_world():
-    return jsonify(hello="world")
+    return jsonify(hello="philips!")
 
 
 @app.route("/static/<path:filename>")
